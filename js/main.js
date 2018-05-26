fb239106dfe60ed56e15e252bd0b6303e0078523
Jorge Verdugo <jorgeverdugoch@gmail.com>
2018-05-25 21:50:50 -0400

    Se modifica el navbar principal

--- js/main.js
+++ js/main.js
@@ -1,48 +1,90 @@
-$(document).ready(function() {
-  var carruselHeader = $('header .owl-carousel');
-  carruselHeader.owlCarousel({
-      items: 1,
-      loop: true,
-      navigation: true,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      autoplayHoverPause: true
-  });
+$(document).ready(function($){
+	var mainHeader = $('.auto-hide-header'),
+		secondaryNavigation = $('.secondary-nav'),
+		//this applies only if secondary nav is below intro section
+		belowNavHeroContent = $('.sub-nav-hero'),
+		headerHeight = mainHeader.height();
 
-  $('.play').on('click',function() {
-      carruselHeader.trigger('play.owl.autoplay',[5000])
-  })
-  $('.stop').on('click',function() {
-      carruselHeader.trigger('stop.owl.autoplay')
-  });
+	//set scrolling variables
+	var scrolling = false,
+		previousTop = 0,
+		currentTop = 0,
+		scrollDelta = 10,
+		scrollOffset = 150;
 
-  var carruselProductos = $('main .owl-carousel');
-  carruselProductos.owlCarousel({
-      items: 4,
-      loop: true,
-      navigation: true,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplayHoverPause: true,
-      margin: 10
-  });
+	mainHeader.on('click', '.nav-trigger', function(event){
+		// open primary navigation on mobile
+		event.preventDefault();
+		mainHeader.toggleClass('nav-open');
+	});
 
-  $('.play').on('click',function() {
-      carruselProductos.trigger('play.owl.autoplay',[3000])
-  })
-  $('.stop').on('click',function() {
-      carruselProductos.trigger('stop.owl.autoplay')
-  });
+	$(window).on('scroll', function(){
+		if( !scrolling ) {
+			scrolling = true;
+			(!window.requestAnimationFrame)
+				? setTimeout(autoHideHeader, 250)
+				: requestAnimationFrame(autoHideHeader);
+		}
+	});
 
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 30) {
-      $('header .main-bar').addClass('fixed-top');
-      $('header .bottom-bar').css("margin-top", "78px")
-    }
+	$(window).on('resize', function(){
+		headerHeight = mainHeader.height();
+	});
 
-    if ($(window).scrollTop() < 30) {
-      $('header .main-bar').removeClass('fixed-top');
-      $('header .bottom-bar').css("margin-top", "0")
-    }
-  });
+	function autoHideHeader() {
+		var currentTop = $(window).scrollTop();
+
+		( belowNavHeroContent.length > 0 )
+			? checkStickyNavigation(currentTop) // secondary navigation below intro
+			: checkSimpleNavigation(currentTop);
+
+	   	previousTop = currentTop;
+		scrolling = false;
+	}
+
+	function checkSimpleNavigation(currentTop) {
+		//there's no secondary nav or secondary nav is below primary nav
+	    if (previousTop - currentTop > scrollDelta) {
+	    	//if scrolling up...
+	    	mainHeader.removeClass('is-hidden');
+	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+	    	//if scrolling down...
+	    	mainHeader.addClass('is-hidden');
+	    }
+	}
+
+	function checkStickyNavigation(currentTop) {
+		//secondary nav below intro section - sticky secondary nav
+		var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
+
+		if (previousTop >= currentTop ) {
+	    	//if scrolling up...
+	    	if( currentTop < secondaryNavOffsetTop ) {
+	    		//secondary nav is not fixed
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.removeClass('fixed slide-up');
+	    		belowNavHeroContent.removeClass('secondary-nav-fixed');
+	    	} else if( previousTop - currentTop > scrollDelta ) {
+	    		//secondary nav is fixed
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.removeClass('slide-up').addClass('fixed');
+	    		belowNavHeroContent.addClass('secondary-nav-fixed');
+	    	}
+
+	    } else {
+	    	//if scrolling down...
+	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
+	 	  		//hide primary nav
+	    		mainHeader.addClass('is-hidden');
+	    		secondaryNavigation.addClass('fixed slide-up');
+	    		belowNavHeroContent.addClass('secondary-nav-fixed');
+	    	} else if( currentTop > secondaryNavOffsetTop ) {
+	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.addClass('fixed').removeClass('slide-up');
+	    		belowNavHeroContent.addClass('secondary-nav-fixed');
+	    	}
+
+	    }
+	}
 });
