e28af22ac4cc7d8e2ac1933816bce83bab0947e0
Jorge Verdugo <jorgeverdugoch@gmail.com>
2018-05-27 20:07:08 -0400

    Se agrega la grilla de porductos

---
+++ js/product.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var carruselProductos = $('main .owl-carousel');
+  carruselProductos.owlCarousel({
+      items: 4,
+      loop: true,
+      navigation: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      margin: 10
+  });
+
+  $('.play').on('click',function() {
+      carruselProductos.trigger('play.owl.autoplay',[3000])
+  })
+  $('.stop').on('click',function() {
+      carruselProductos.trigger('stop.owl.autoplay')
+  });
+});
